24c3b26c32bbe204fd17b4dfccc17ba77998126c
hacknus <linus.stoeckli@unibe.ch>
2025-05-27 23:57:09 +0200

    remove dash

--- thz_database/settings.py
+++ thz_database/settings.py
@@ -35,7 +35,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django_plotly_dash.middleware.BaseMiddleware',  # For Dash
     # ...
 ]
 

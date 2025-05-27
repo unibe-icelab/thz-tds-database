e52c73eff03544475b997daff73f18f8f1e09015
hacknus <linus.stoeckli@unibe.ch>
2025-05-27 22:50:53 +0200

    implement pw change

--- spectra/urls.py
+++ spectra/urls.py
@@ -1,6 +1,7 @@
 # spectra/urls.py
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views # Import Django's auth views
 
 app_name = 'spectra'
 
@@ -13,4 +14,22 @@ urlpatterns = [
     path('<int:pk>/download/', views.download_spectrum_file, name='download_spectrum_file'),
     path('spectrum/upload/', views.upload_spectrum, name='upload_spectrum'),
     path('token/regenerate/', views.regenerate_token_view, name='regenerate_token'),
+
+    # path('api/upload/', views.api_upload_spectrum, name='api_upload_spectrum'),
+    # path('api/spectra/', views.api_spectrum_list, name='api_spectrum_list'),
+    # path('api/spectra/<int:pk>/', views.api_spectrum_detail, name='api_spectrum_detail'),
+    # path('api/spectra/<int:pk>/download/', views.api_download_spectrum_file, name='api_download_spectrum_file'),
+
+    # Password Change URLs
+    path('password_change/',
+         auth_views.PasswordChangeView.as_view(
+             template_name='registration/password_change_form.html', # Custom template
+             success_url='/password_change/done/' # Custom success URL
+         ),
+         name='password_change'),
+    path('password_change/done/',
+         auth_views.PasswordChangeDoneView.as_view(
+             template_name='registration/password_change_done.html' # Custom template
+         ),
+         name='password_change_done'),
 ]
